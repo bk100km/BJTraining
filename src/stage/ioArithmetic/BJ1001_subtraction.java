12e9c1bd608c205b61f1138647180a5370ebb7f4
bk100km <bk100km@naver.com>
2023-06-15 22:02:05 +0900

    Project init

---
+++ src/stage/ioArithmetic/BJ1001_subtraction.java
@@ -0,0 +1,16 @@
+package stage.ioArithmetic;
+
+import java.util.Scanner;
+
+public class BJ1001_subtraction {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int a = Integer.parseInt(scanner.next());
+            int b = Integer.parseInt(scanner.next());
+            System.out.println(a - b);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/stage/ioArithmetic/BJ2557_HelloWorld.java
@@ -0,0 +1,7 @@
+package stage.ioArithmetic;
+
+public class BJ2557_HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
